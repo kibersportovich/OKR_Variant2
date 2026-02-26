1c39e0d3c975499e8fbc33286adb5c2692651e11
kibersportovich <kirilenckopashayandex.ru@gmail.com>
2026-02-26 18:53:39 +0300

    task 3

--- okr.js
+++ okr.js
@@ -17,6 +17,31 @@ function printMultiplesOfThree() {
 
 printMultiplesOfThree();
 
+//task 3
+
+function getOldestPerson(people) {
+    let oldestAge = -Infinity;
+    let oldestPerson = '';
+  
+    for (const [name, age] of Object.entries(people)) {
+      if (age > oldestAge) {
+        oldestAge = age;
+        oldestPerson = name;
+      }
+    }
+  
+    return oldestPerson;
+}
+
+const people = {
+    "Иван": 25,
+    "Мария": 32,
+    "Алексей": 40,
+    "Ольга": 29
+};
+  
+console.log(getOldestPerson(people)); 
+
 
 
 
